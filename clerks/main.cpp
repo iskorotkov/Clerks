99e4bbd359aeba4eba93d14bf6884e46cdb782af
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-03-30 19:23:08 +0500

    Start from scratch

--- clerks/main.cpp
+++ clerks/main.cpp
@@ -1,9 +1,3 @@
-// 1. Read clerks data and store it in search tree
-// 2. Build usual tree from search tree (or vector)
-// 3. Recursively find best clerk path to the bottom
-// 4. Move through chief ptrs to restore path from bottom to top
-// 5. Free memory used by tree
-
 #include <iostream>
 
 int main()
