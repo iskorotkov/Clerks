d0ffc91474552fcfd5327bdfb1b02e21404d385d
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-04-10 18:00:54 +0500

    Refactor code: encapsulate fields and add accessors methods

--- clerks/main.cpp
+++ clerks/main.cpp
@@ -3,56 +3,94 @@
 #include <algorithm>
 #include <iostream>
 
-struct clerk
+class clerk
 {
-	int bribe = 0;
-	int number = 1;
-	std::vector<const clerk*> subordinates;
+public:
+	auto& subordinates() { return subordinates_; }
+	auto bribe(const int value) { bribe_ = value; }
+	auto number(const int value) { number_ = value; }
+
+	auto& subordinates() const { return subordinates_; }
+	auto bribe() const { return bribe_; }
+	auto number() const { return number_; }
+
+private:
+	int bribe_ = 0;
+	int number_ = 1;
+	std::vector<const clerk*> subordinates_;
 };
 
-struct path
+class path
 {
-	int spent = 0;
-	std::vector<const clerk*> clerks;
+public:
+	auto& clerks() { return clerks_; }
+	auto spent(const int value) { return spent_ = value; }
+
+	auto& clerks() const { return clerks_; }
+	auto spent() const { return spent_; }
+	auto empty() const { return clerks_.empty(); }
+
+private:
+	int spent_ = 0;
+	std::vector<const clerk*> clerks_;
 };
 
-struct clerk_data
+class clerk_data
 {
-	int bribe = 0;
-	int number = 1;
-	std::vector<int> subordinates;
+public:
+	auto& subordinates() { return subordinates_; }
+	auto bribe(const int value) { bribe_ = value; }
+	auto number(const int value) { number_ = value; }
+
+	auto& subordinates() const { return subordinates_; }
+	auto bribe() const { return bribe_; }
+	auto number() const { return number_; }
+
+private:
+	int bribe_ = 0;
+	int number_ = 1;
+	std::vector<int> subordinates_;
 };
 
-struct parse_data
+class parse_data
 {
-	int chief_index = -1;
-	std::vector<clerk_data> clerks;
+public:
+	auto& clerks() { return clerks_; }
+	auto chief_index(const int value) { chief_index_ = value; }
+
+	auto& clerks() const { return clerks_; }
+	auto chief_index() const { return chief_index_; }
+	auto has_chief() const { return chief_index_ >= 0; }
+
+private:
+	int chief_index_ = -1;
+	std::vector<clerk_data> clerks_;
 };
 
 path best_path(const clerk* chief)
 {
 	path result;
-	if (!chief->subordinates.empty())
+	if (!chief->subordinates().empty())
 	{
-		for (const auto& c : chief->subordinates)
+		for (const auto& c : chief->subordinates())
 		{
 			const auto p = best_path(c);
-			if (result.spent == 0 || p.spent < result.spent)
+			if (result.empty() || p.spent() < result.spent())
 			{
 				result = p;
 			}
 		}
 	}
-	result.spent += chief->bribe;
-	result.clerks.push_back(chief);
+	result.spent(result.spent() + chief->bribe());
+	result.clerks().push_back(chief);
 	return result;
 }
 
 void print_result(const path& p)
 {
 	std::ofstream output("output.txt");
-	output << p.spent << '\n';
-	std::for_each(p.clerks.cbegin(), p.clerks.cend(), [&output](const auto& c) { output << c->number << ' '; });
+	output << p.spent() << '\n';
+	std::for_each(p.clerks().cbegin(), p.clerks().cend(), [&output](const auto& c) { output << c->number() << ' '; });
 }
 
 parse_data parse_input()
@@ -61,27 +99,27 @@ parse_data parse_input()
 	int n;
 	input >> n;
 	parse_data data;
-	data.clerks.resize(n);
+	data.clerks().resize(n);
 	for (auto i = 0; i < n; ++i)
 	{
 		int number, chief, bribe;
 		input >> number >> chief >> bribe;
-		data.clerks.at(number - 1).number = number;
-		data.clerks.at(number - 1).bribe = bribe;
+		data.clerks().at(number - 1).number(number);
+		data.clerks().at(number - 1).bribe(bribe);
 		if (chief > 0)
 		{
-			data.clerks.at(chief - 1).subordinates.emplace_back(number - 1);
+			data.clerks().at(chief - 1).subordinates().emplace_back(number - 1);
 		}
 		else
 		{
-			if (data.chief_index >= 0)
+			if (data.has_chief())
 			{
 				throw std::exception("—уществует более одного чиновника без начальника");
 			}
-			data.chief_index = number - 1;
+			data.chief_index(number - 1);
 		}
 	}
-	if (data.chief_index < 0)
+	if (!data.has_chief())
 	{
 		throw std::exception("Ќет ни одного чиновника без начальника");
 	}
@@ -91,43 +129,37 @@ parse_data parse_input()
 clerk* create_clerk(const clerk_data& data)
 {
 	const auto cl = new clerk;
-	cl->number = data.number;
-	cl->bribe = data.bribe;
+	cl->number(data.number());
+	cl->bribe(data.bribe());
 	return cl;
 }
 
 clerk* create_subtree(const std::vector<clerk_data>& data, const int index)
 {
-	const auto& chief_data = data[index];
+	const auto& chief_data = data.at(index);
 	const auto chief = create_clerk(chief_data);
-	for (const auto subordinate : chief_data.subordinates)
+	for (const auto subordinate : chief_data.subordinates())
 	{
-		chief->subordinates.emplace_back(create_subtree(data, subordinate));
+		chief->subordinates().emplace_back(create_subtree(data, subordinate));
 	}
 	return chief;
 }
 
 clerk* create_tree(const parse_data& data)
 {
-	return create_subtree(data.clerks, data.chief_index);
+	return create_subtree(data.clerks(), data.chief_index());
 }
 
 void clear_tree(const clerk* chief)
 {
 	if (chief)
 	{
-		for (const auto& cl : chief->subordinates)
+		for (const auto& cl : chief->subordinates())
 		{
 			clear_tree(cl);
 		}
 		delete chief;
 	}
-}
-
-void log_error(const std::exception& e)
-{
-	std::ofstream output("output.txt");
-	output << e.what();
 }
 
 int main()
@@ -142,6 +174,7 @@ int main()
 	}
 	catch (const std::exception& e)
 	{
-		log_error(e);
+		std::ofstream output("output.txt");
+		output << e.what();
 	}
 }
